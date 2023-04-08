72c71e00d96427a4584d8744708177f48b1aa4da
Ikromjon <ikromjon98.98@icloud.com>
2023-04-08 22:22:21 +0200

    completed

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const shortid = require('shortid');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const urls = {};
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,6 +24,34 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+// Endpoint to handle URL shortening
+app.post('/api/shorturl', function(req, res) {
+  const originalUrl = req.body.url;
+  const parsedUrl = new URL(originalUrl);
+  
+  // Validate the URL
+  dns.lookup(parsedUrl.hostname, function(err, address, family) {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      const shortUrl = shortid.generate();
+      urls[shortUrl] = originalUrl;
+      res.json({ original_url: originalUrl, short_url: shortUrl });
+    }
+  });
+});
+
+// Endpoint to handle redirection
+app.get('/api/shorturl/:id', function(req, res) {
+  const shortUrl = req.params.id;
+  const originalUrl = urls[shortUrl];
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.json({ error: 'invalid url' });
+  }
 });
 
 app.listen(port, function() {
